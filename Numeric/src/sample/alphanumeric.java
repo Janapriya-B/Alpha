1537bfec3a47b25751d29c22ffd7f94687b14f5c
janapriya <janapriya@Janapriya-PC>
2020-01-20 16:27:27 +0530

    Files committed

---
+++ Numeric/src/sample/alphanumeric.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class alphanumeric {
+
+	public static void main(String[] args) {
+		System.out.println("sample numeric");
+		// TODO Auto-generated method stub
+
+	}
+
+}
