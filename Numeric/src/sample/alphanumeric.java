71a442cd6f2efa6203090bcd7697c74fd2ae21f4
janapriya <janapriya@Janapriya-PC>
2020-01-22 13:23:11 +0530

    File Updated

--- Numeric/src/sample/alphanumeric.java
+++ Numeric/src/sample/alphanumeric.java
@@ -1,9 +1,20 @@
 package sample;
 
+import java.util.Scanner;
+
 public class alphanumeric {
 
 	public static void main(String[] args) {
-		System.out.println("sample numeric");
+		int a,c;
+		System.out.println("Enter 2 integers");
+		Scanner m= new Scanner(System.in);
+		a=m.nextInt();
+		c=m.nextInt();
+		//c=m.nextInt();
+		
+		
+		
+		
 		// TODO Auto-generated method stub
 
 	}
